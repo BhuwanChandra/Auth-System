31206e9363039a3f96850c75990510d8953ae3a5
BhuwanChandra <bhuwan7779@gmail.com>
2020-05-08 15:54:38 +0530

    updated

--- client/src/redux/actions.js
+++ client/src/redux/actions.js
@@ -1,91 +1,84 @@
 import {
-    USER_REQUEST,
-    USER_FAILURE,
-    AUTHENTICATE_USER,
-    SIGNUP_USER_SUCCESS,
-    LOGOUT_USER
-} from './types';
+  USER_REQUEST,
+  USER_FAILURE,
+  AUTHENTICATE_USER,
+  SIGNUP_USER_SUCCESS,
+  LOGOUT_USER
+} from "./types";
+import axios from "axios";
 
 export const userRequest = () => {
-    return {
-        type: USER_REQUEST
-    }
-}
+  return {
+    type: USER_REQUEST
+  };
+};
 
-export const userFailure = (err) => {
-    return {
-        type: USER_FAILURE,
-        payload: err
-    }
-}
+export const userFailure = err => {
+  return {
+    type: USER_FAILURE,
+    payload: err
+  };
+};
 
 export const signupUserSuccess = () => {
-    return {
-        type: SIGNUP_USER_SUCCESS
-    }
-}
+  return {
+    type: SIGNUP_USER_SUCCESS
+  };
+};
 
-export const authenticateUser = (userDetails) => {
-    return {
-        type: AUTHENTICATE_USER,
-        payload: userDetails
-    }
-}
+export const authenticateUser = userDetails => {
+  return {
+    type: AUTHENTICATE_USER,
+    payload: userDetails
+  };
+};
 
 export const logoutUser = () => {
-    return {
-        type: LOGOUT_USER
-    }
-}
+  return {
+    type: LOGOUT_USER
+  };
+};
 
-export const signupUser = (userData) => {
-    return (dispatch) => {
-        dispatch(userRequest())
-        return fetch(`/signup`,{
-            method: "post",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify(userData)
-        })
-        .then(res => res.json())
-        .then(res => {
-            if(!res.error)
-            dispatch(signupUserSuccess());
-            else
-            dispatch(userFailure(res.error));
-        })
-        .catch(error => {
-            const errorMsg = error.message;
-            dispatch(userFailure(errorMsg));
-        })
-    }
-}
+export const signupUser = userData => {
+  return dispatch => {
+    dispatch(userRequest());
+    return axios
+      .post(`/signup`, userData, {
+        headers: {
+          "Content-Type": "application/json"
+        }
+      })
+      .then(res => {
+        console.log(res.data);
+        if (!res.error) dispatch(signupUserSuccess());
+        else dispatch(userFailure(res.data.error));
+      })
+      .catch(error => {
+        const errorMsg = error.message;
+        dispatch(userFailure(errorMsg));
+      });
+  };
+};
 
-export const loginUser = (userData) => {
-    return (dispatch) => {
-        dispatch(userRequest());
-        return fetch(`/login`,{
-            method: "post",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify(userData)
-        })
-        .then(res => res.json())
-        .then(response => {
-            if(!response.error){
-                localStorage.setItem('user', JSON.stringify(response.user));
-                localStorage.setItem('token', JSON.stringify(response.token));
-                dispatch(authenticateUser(response));
-            }
-            else
-                dispatch(userFailure(response.error));
-        })
-        .catch(error => {
-            const errorMsg = error.message;
-            dispatch(userFailure(errorMsg));
-        })
-    }
-}
-
+export const loginUser = userData => {
+  return dispatch => {
+    dispatch(userRequest());
+    return axios
+      .post(`/login`, userData, {
+        headers: {
+          "Content-Type": "application/json"
+        }
+      })
+      .then(response => {
+        if (!response.data.error) {
+          localStorage.setItem("user", JSON.stringify(response.data.user));
+          localStorage.setItem("token", JSON.stringify(response.data.token));
+          dispatch(authenticateUser(response.data));
+        } else dispatch(userFailure(response.data.error));
+      })
+      .catch(error => {
+        const errorMsg = error.message;
+        dispatch(userFailure(errorMsg));
+      });
+  };
+};
--- index.js
+++ index.js
@@ -2,11 +2,9 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
-const { MONGO_URI, JWT_SECRET} = require('./config/keys');
+const { MONGO_URI } = require('./config/keys');
 const PORT = process.env.PORT || 8080;
-const User = require('./user');
+const User = require('./models/user');
 
 app.use(bodyParser.json());
 
@@ -20,91 +18,8 @@ mongoose.connection.on("connected", () => {
     console.log("connected to database!!!");
 });
 
-app.post("/signup", (req, res) => {
-  const { name, email, password } = req.body;
-  if (!email || !name || !password) {
-    return res.status(422).json({
-      error: "please add all the fields."
-    });
-  }
-  User.findOne({ email: email })
-    .then(saveUser => {
-      if (saveUser)
-        return res.status(422).json({
-          error: "Email already registered."
-        });
-
-      bcrypt
-        .hash(password, 12)
-        .then(hashPassword => {
-          const user = new User({
-            email,
-            password: hashPassword,
-            name
-          });
-          user
-            .save()
-            .then(user => {
-              res.json({ message: "user saved successfully" });
-            })
-            .catch(err => {
-                console.log(err);
-                return res.status(500).json({error: "Something went wrong!"})
-            });
-        })
-        .catch(err => {
-            console.log(err);
-            return res.status(500).json({error: "Something went wrong!"})
-        });
-    })
-    .catch(err => {
-        console.log(err);
-        return res.status(500).json({error: "Something went wrong!"})
-    });
-});
-
-app.post("/login", (req, res) => {
-  const { email, password } = req.body;
-  if (!email || !password)
-    return res.status(422).json({
-      error: "please add all the fields."
-    });
-
-  User.findOne({ email: email })
-    .then(savedUser => {
-      if (!savedUser)
-        return res.status(422).json({
-          error: "Invalid Email or Password."
-        });
-
-      bcrypt
-        .compare(password, savedUser.password)
-        .then(doMatch => {
-          if (doMatch) {
-            const token = jwt.sign({ _id: savedUser._id }, JWT_SECRET);
-            const { _id, name, email } = savedUser;
-            res.json({
-              message: "Successfully signed in!!!",
-              token,
-              user: {_id, name, email}
-            });
-          } else {
-            return res
-              .status(422)
-              .json({ error: "Invalid Email or Password." });
-          }
-        })
-        .catch(err => {
-            console.log(err)
-            return res.status(500).json({error: "Something went wrong!"})
-        });
-    })
-    .catch(err => {
-        console.log(err)
-        return res.status(500).json({error: "Something went wrong!"})
-    });
-});
-
+app.use(require('./routes/auth'));
+app.use(require('./routes/user'));
 
 if(process.env.NODE_ENV == "production"){
   const path = require('path');
