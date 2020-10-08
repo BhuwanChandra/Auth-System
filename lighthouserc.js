ba20e35f709ba8c3ac47d8353a8d18fa9f830e6d
BhuwanChandra <bhuwan7779@gmail.com>
2020-10-09 00:29:26 +0530

    updated lighthouse ci

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,5 +1,10 @@
 module.exports = {
   ci: {
+    // Static site example
+    collect: {
+      startServerCommand: "npm run start",
+      url: ["https://auth-system-7779.herokuapp.com/"],
+    },
     upload: {
       target: "temporary-public-storage",
     },
