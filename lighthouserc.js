918fa1492b51f14b90a9ff6c68756320a2c25b7b
BhuwanChandra <bhuwan7779@gmail.com>
2020-10-09 01:01:17 +0530

    updated

--- lighthouserc.js
+++ lighthouserc.js
@@ -2,6 +2,7 @@ module.exports = {
   ci: {
     // Static site example
     collect: {
+      numberOfRuns: 3,
       startServerCommand: "cd client && npm run start",
       url: ["https://auth-system-7779.herokuapp.com/"],
     },
