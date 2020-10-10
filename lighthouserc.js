7ee0254d70e8c03ae19aee3b09db4b7a441aef65
BhuwanChandra <bhuwan7779@gmail.com>
2020-10-10 09:47:20 +0530

    added asssertions

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,6 +1,5 @@
 module.exports = {
   ci: {
-    // Static site example
     collect: {
       numberOfRuns: 3,
       startServerCommand: "cd client && npm run start",
@@ -8,6 +7,17 @@ module.exports = {
     },
     upload: {
       target: "temporary-public-storage",
+    },
+    assert: {
+      assertions: {
+        "categories:accessibility": ["error", { minScore: 0.85 }],
+        "categories:best-practices": ["error", { minScore: 0.8 }],
+        "categories:performance": ["error", { minScore: 0.75 }],
+        "categories:seo": ["warn", { minScore: 0.8 }],
+        "dom-size": ["warn", { maxNumericValue: 1000 }],
+        "first-contentful-paint": ["error", { maxNumericValue: 2500 }],
+        interactive: ["error", { maxNumericValue: 5000 }],
+      },
     },
   },
 };
